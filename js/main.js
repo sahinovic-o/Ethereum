5b3987277089ec0516b31bc711a3b562e429236e
Bob <sahinovicomar@gmail.com>
2022-02-13 19:00:31 +0100

    Ethereum

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var typed = new Typed('.element', {
+    strings: ["Arabic", "Chinese", "Croatian", "Dutch", "Finnish", "French","Itlian","Japanese", "English"],
+    showCursor: false,
+    typeSpeed: 50,
+    backSpeed: 50,
+    smartBackspace: true,
+    loop: true,
+    loopCount: Infinity
+});
